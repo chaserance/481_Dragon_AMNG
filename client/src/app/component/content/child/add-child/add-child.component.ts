412d9c2e2b94fd79f9563962f38dd1f681d721a6
ddreimane <dreimane.dace@gmail.com>
2017-12-09 19:39:14 -0500

    change

--- client/src/app/component/content/child/add-child/add-child.component.ts
+++ client/src/app/component/content/child/add-child/add-child.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+ import {Component, OnInit} from '@angular/core';
+ import { FormBuilder, Validators } from '@angular/forms';
+ import {AuthService} from '../../../service/auth.service';
+import {Response} from '@angular/http';
+import {Router} from '@angular/router';
+
+import { RequestExceptionHandler } from '../../../error/request-exception-handler';
+
+import { State } from '../../../model/state';
+import {LoginRequest} from '../../../model/login-request';
+import {ValidatorsService} from '../../../service/validators.service';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import {UserService} from '../../../service/user.service';
 
 @Component({
   selector: 'app-add-child',
   templateUrl: './add-child.component.html',
   styleUrls: ['./add-child.component.scss']
 })
-export class AddChildComponent implements OnInit {
 
-  constructor() { }
+ export class AddChildComponent implements OnInit {
 
-  ngOnInit() {
+  form: any;
+  loading = false;
+  error = [];
+  states = State;
+
+  stateKeys(): Array<string> {
+    const keys = Object.keys(this.states);
+    return keys.slice(keys.length / 2);
   }
+
+   constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private validator: ValidatorsService,
+    private userService: UserService
+  ) { }
+//
+  ngOnInit() {
+    this.form = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required]
+
+    },);
+   }
+
+  onSubmit() {
+    this.auth.child(this.form.value as LoginRequest).subscribe(
+      (response: Response) => {
+        if (response.status === 201) {
+          this.router.navigate(['/login']);
+        } else {
+          this.error = response.json().message();
+        }
+      },
+      error => {
+        // Bad request
+        if (error.status === 400 && !!!error.json().errorCode) {
+          const errorArray = RequestExceptionHandler.getBadRequestMessages(error.json().errors);
+          this.error = errorArray;
+        } else {
+          this.error = [];
+          this.error.push(error.json().message);
+        }
+      }
+    );
+  }
+
+  showError(formControl): boolean {
+   return formControl.invalid && (formControl.dirty || formControl.touched);
+  }
+
+  closeAlert(): void {
+   this.error = [];
+  }
+
+
+
+  get firstname() { return this.form.get('firstname'); }
+  get lastname() { return this.form.get('lastname'); }
+
 
 }
